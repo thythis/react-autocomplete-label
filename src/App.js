bbe1fa7a5932e5c2fe910797cab29041a39fd315
leotang <v-leotang@expedia.com>
2020-08-07 23:44:08 +0800

    :tada:  init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-lg-6 col-lg-offset-3">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search for..."
+            />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="button">
+                Go!
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
